3e1e548adb6e1a47cf37bcbfccc55fc76d3e5033
Tsonko <t_stoianov@yahoo.com>
2019-07-08 12:16:49 +0300

    added front end

--- Messages/Messages/Startup.cs
+++ Messages/Messages/Startup.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Messages.Data;
+﻿using Messages.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Messages
 {
@@ -38,7 +31,7 @@ namespace Messages
                 options.AddPolicy("MessagesCORSPolicy",
                     builder =>
                     {
-                        builder.WithOrigins("http://localhost:8001")
+                        builder.WithOrigins("http://localhost:5500")
                                 .AllowAnyHeader();
                     });
             });
@@ -66,6 +59,7 @@ namespace Messages
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseCors("MessagesCORSPolicy");
 
             app.UseHttpsRedirection();
